fe38666782ce0cbbfc8aa44324fea97dd93fef95
Roshanzs <callmezs@163.com>
2026-04-12 09:14:50 +1200

    commit

---
+++ MSE800-PSD/WEEK1/power.py
@@ -0,0 +1,10 @@
+
+#calcuator power function
+def power(x, y):
+    print(f"x: {x}, y: {y} = {x ** y}")
+
+if __name__ == "__main__":
+    x = int(input("Enter the x: "))
+    y = int(input("Enter the y: "))
+    power(x, y)
+    
