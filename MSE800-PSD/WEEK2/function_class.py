c31b47a9b0017145818ae4e6a42f88e0fbe3e547
Roshanzs <callmezs@163.com>
2026-04-19 09:33:39 +1200

    add comments and  fix some bugs

--- MSE800-PSD/WEEK2/function_class.py
+++ MSE800-PSD/WEEK2/function_class.py
@@ -1,15 +1,19 @@
 
+#define a grouble variable to store student information
 students = []
 
+#define a class to represent a student
 class Student:
     def __init__(self, name, age, student_id):
         self.name = name
         self.age = age
         self.student_id = student_id
 
+    #function to display student information
     def display_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
 
+#function to collect student information
 def collect_info():
     for i in range(3):
         student_str = f"Student {i + 1}"
@@ -19,9 +23,9 @@ def collect_info():
         student = Student(name, age, student_id)
         students.append(student)
 
+#function to display students in order
 def display_students():
-    for student in students:
-        sorted_students = sorted(students, key=lambda s: s.name)
+    sorted_students = sorted(students, key=lambda s: s.name)
     for student in sorted_students:
         student.display_info()
 
