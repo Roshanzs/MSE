a02b3886924b9925704bce648db9509a12b0e462
Roshanzs <callmezs@163.com>
2026-05-08 15:54:51 +1200

    update

--- MSE802-QC/WEEK4/temptesk.py
+++ MSE802-QC/WEEK4/temptesk.py
@@ -4,14 +4,12 @@ from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 from qiskit.visualization import plot_state_qsphere
 
-# 创建 2-qubit 电路
+# create a quantum circuit with 2 qubits
 qc1 = QuantumCircuit(2)
-# 先将 qubit 0 变为叠加态，否则相位移不可见
 qc1.h(0)
-# 应用 90 度相位移 (pi/2)
 qc1.p(np.pi/2, 0) 
 
-# 获取状态向量并绘图
+# get the state vector and plot the qsphere
 state = Statevector.from_instruction(qc1)
 plot_state_qsphere(state)
 # %%
